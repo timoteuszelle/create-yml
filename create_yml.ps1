8c575f4f80434b1e9a46d4a38a34986615625cb3
Jaap Brasser <noreply@jaapbrasser.com>
2021-01-27 14:10:59 +0100

    I needed some indentation, so I added that 🦄

--- create_yml.ps1
+++ create_yml.ps1
@@ -5,6 +5,6 @@ needed some .yml templates
 
 $file = (Get-Content -Path c:\temp\files\lijst.txt)
 $text = (Get-Content -path c:\temp\files\template.txt)
-foreach ($line in $file) {$item = (New-Item -path c:\temp\files\$line.yml -ItemType file)
-Add-Content $item ($text)
+    foreach ($line in $file) {$item = (New-Item -path c:\temp\files\$line.yml -ItemType file)
+    Add-Content $item ($text)
 }
