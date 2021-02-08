3aee8c0e9267ac24a7fa00f32a98831a95b4e0a5
Tim Zelle <tim.zelle@sublie.me>
2021-02-08 14:51:03 +0100

    formatting done

--- create_yml.ps1
+++ create_yml.ps1
@@ -2,16 +2,15 @@
 Author: Tim Zelle
 needed some .yml templates
 #> 
-[int]$counter = (Read-Host "Type the last known sequence number, for example  41")
-[int]$nexseq  = ($counter +[int]1) 
-
 $file = (Get-Content -Path c:\temp\files\lijst.txt)
 $text = (Get-Content -path c:\temp\files\template.txt)
+[int]$counter = (Read-Host "Type the last known used sequence number. (for example 41, the next one will be 042-somthing.yml")
+[int]$nexseq  = ($counter +[int]1) 
+
     foreach ($line in $file) {
     $i =  "{0:D3}" -f $nexseq++
     [string]$seq = $i
     $filename =  "$seq-$line.yml"
     $item = (New-Item -path c:\temp\files\$filename -ItemType file)
     Add-Content $item ($text)
-    
 }
