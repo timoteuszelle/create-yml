7a30c748c88130cd77beb76e97fafc699ba26101
Tim Zelle <tim.zelle@sublie.me>
2021-02-08 10:40:38 +0100

    added sequence numbering

--- create_yml.ps1
+++ create_yml.ps1
@@ -2,9 +2,16 @@
 Author: Tim Zelle
 needed some .yml templates
 #> 
+[int]$counter = (Read-Host "Type the last known sequence number, for example  41")
+[int]$nexseq  = ($counter +[int]1) 
 
 $file = (Get-Content -Path c:\temp\files\lijst.txt)
 $text = (Get-Content -path c:\temp\files\template.txt)
-foreach ($line in $file) {$item = (New-Item -path c:\temp\files\$line.yml -ItemType file)
-Add-Content $item ($text)
+    foreach ($line in $file) {
+    $i =  "{0:D3}" -f $nexseq++
+    [string]$seq = $i
+    $filename =  "$seq-$line.yml"
+    $item = (New-Item -path c:\temp\files\$filename -ItemType file)
+    Add-Content $item ($text)
+    
 }
